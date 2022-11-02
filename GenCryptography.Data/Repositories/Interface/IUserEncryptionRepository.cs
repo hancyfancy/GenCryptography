92932077b48345f9a84d33795848d05e189eb745
hancyfancy <20492210+hancyfancy@users.noreply.github.com>
2022-11-02 13:32:49 +1100

    Update IUserEncryptionRepository.cs

--- GenCryptography.Data/Repositories/Interface/IUserEncryptionRepository.cs
+++ GenCryptography.Data/Repositories/Interface/IUserEncryptionRepository.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using GenCryptography.Data.Models;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -10,6 +11,6 @@ namespace GenCryptography.Data.Repositories.Interface
     {
         byte[] InsertOrUpdate(long userId, byte[] encryptionKey);
 
-
+        UserEncryption Get(long userId);
     }
 }
